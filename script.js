4bac0b442e2b8bcd499830104a279679cf60ad59
real-world-max <sheikhkhan0089@gmail.com>
2026-06-08 15:32:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ console.log("Living Forest V1 Loaded");
 // - weather system
 // - movement system
 // - survival system
+let player = document.getElementById("player");
+
+let x = window.innerWidth / 2;
+let y = window.innerHeight / 2;
+let speed = 5;
+
+document.addEventListener("keydown", function(e){
+
+if(e.key === "ArrowUp"){
+y -= speed;
+}
+
+if(e.key === "ArrowDown"){
+y += speed;
+}
+
+if(e.key === "ArrowLeft"){
+x -= speed;
+}
+
+if(e.key === "ArrowRight"){
+x += speed;
+}
+
+player.style.left = x + "px";
+player.style.top = y + "px";
+
+});
